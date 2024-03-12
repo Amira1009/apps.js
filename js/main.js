e3821af7bcd68fed560128209bedc227ced132ac
Amira1009 <am2819497@gmail.com>
2024-03-13 01:19:15 +0200

    commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var color = "blue";
+console.log(color , typeof 'color' );
+
+var newsLetter = true;
+console.log(newsLetter , typeof newsLetter)
+
+var calculating = 140;
+console.log(calculating , typeof calculating);
+
+var welcomeMassage = "welcome Massage";
+console.log(welcomeMassage , typeof 'welcomeMassage');
+
+var password = "Amira123";
+console.log(password , typeof password);
+
+var trackOfTheNumbe = 2;
+console.log(trackOfTheNumbe , typeof trackOfTheNumbe) ;
